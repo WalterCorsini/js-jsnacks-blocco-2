6cf8d5cf9b38da96527ba2f5aed2b9342add04b1
WalterCorsini <corsini.walter@gmail.com>
2024-04-05 11:53:22 +0200

    complete jsnack1

--- js/function.js
+++ js/function.js
@@ -1,11 +1,15 @@
 
 function rndGen(min,max,number){
+    let array = [];
+
+
     for(let i = 0; i<number; i++){
         let result = "";
         let rndNumber = 0;
         rndNumber = Math.floor(Math.random() * (max - min +1)+1);
-        
+        array.push(rndNumber);
     }
+    console.log(array);
 
-    return result;
+    return array;
 }
