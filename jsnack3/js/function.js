20501df03ca8a97d45adacde9f9d9dbcb3556f29
WalterCorsini <corsini.walter@gmail.com>
2024-04-05 18:37:30 +0200

    fix: jsnack-3

--- jsnack3/js/function.js
+++ jsnack3/js/function.js
@@ -6,7 +6,12 @@
 function stampa(arrayText){
     let result = "";
     for(i = 0; i<arrayText.length; i++){
+        if(i <arrayText.length-1){
         result += arrayText[i] + ", ";
+        } else {
+        result += arrayText[i];
+
+        }
     }
     return result;
 }
