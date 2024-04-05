e8966be2a043603f80cf85704bc40dd3a27a34a6
WalterCorsini <corsini.walter@gmail.com>
2024-04-05 12:47:55 +0200

    complete jsnack2

--- jsnack2/js/function.js
+++ jsnack2/js/function.js
@@ -3,12 +3,9 @@ function longWord(text1,text2){
     console.log(text1.length, text2.length);
     if(text1.length > text2.length){
         return result = text1;
-        alert("parola 1");
     } else if (text1.length < text2.length){
         return result = text2;
-        alert("parola 2");
     } else {
         return result ="uguale";
-        alert("uguale");
     }
 }
