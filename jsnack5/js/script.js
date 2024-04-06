d2ddbeab4aa91adfd38dbc61f550146afa428807
WalterCorsini <corsini.walter@gmail.com>
2024-04-06 11:32:26 +0200

    jsnack-5  insert result on page and  add comment

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,8 +1,8 @@
 // INPUT
 // declaration variable adn array
-const array = ["pasta","pane","CAFFE","vino"];
-console.log(array);
-// LOGIC
-const arrayLessOne = rimuoviDallaTesta(array);
-// OUTPUT
-console.log("primo elemento eliminato. il tuo array aggiornato è: " + arrayLessOne);
+const array = ["pasta","pane","CAFFE","vino"];                                                                                  // array 
+// LOGIC and OUTPUT
+document.getElementById("result").innerHTML = "<h2>il tuo array è: </h2>" + array;                                              // insert on page original array
+document.getElementById("result").innerHTML += "<h2>il tuo array senza il primo elemento è</h2>" + rimuoviDallaTesta(array);     // insert on page original array less the first element
+console.log("ARRAY ORIGINALE",array);                                                                                           // show in console original array
+console.log("ARRAY SENZA ULTIMO ELEMENTO",rimuoviDallaTesta(array));                                                             // show in console orring array less the first element
