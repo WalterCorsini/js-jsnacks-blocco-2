e0f554580f3f551bb08d7b075c7d5e1e5809186d
WalterCorsini <corsini.walter@gmail.com>
2024-04-06 11:29:24 +0200

    jsnack-4  insert result on page and  add comment

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,8 +1,9 @@
 // INPUT
 // declaration variable adn array
-const array = ["pasta","pane","CAFFE","vino"];
-console.log(array);
-// LOGIC
-const arrayLessOne = rimuoviDallaCoda(array);
-// OUTPUT
-console.log("ultimo elemento eliminato. il tuo array aggiornato è: " + arrayLessOne);
+const array = ["pasta","pane","CAFFE","vino"];                                                                                  // array 
+// LOGIC and OUTPUT
+document.getElementById("result").innerHTML = "<h2>il tuo array è: </h2>" + array;                                              // insert on page original array
+document.getElementById("result").innerHTML += "<h2>il tuo array senza l'ultimo elemento è</h2>" + rimuoviDallaCoda(array);     // insert on page original array less the last element
+console.log("ARRAY ORIGINALE",array);                                                                                           // show in console original array
+console.log("ARRAY SENZA ULTIMO ELEMENTO",rimuoviDallaCoda(array));                                                             // show in console orring array less the last element
+
